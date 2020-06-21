318985139794337b51403a284b08857a7a409cab
ryu shiroma <ryushiroma@gmail.com>
2020-06-21 16:25:37 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+/*Desafio 2 - Exercício 4*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const contactArray = [
+    { name: 'Diego' },
+    { name: 'Gabriel' },
+    { name: 'Lucas' }
+  ]
+
+  class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { contacts: contactArray };
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleSubmit(event) {
+      event.preventDefault();
+      const { contacts } = this.state,
+        name = this.refs.name.value;
+      this.setState({
+        contacts: [...contacts, { name }]
+      }, () => { this.refs.name.value = '' });
+    }
+    render() {
+      const { contacts } = this.state;
+
+      return (
+        <div>
+          <h2>Add Someone</h2>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" ref="name" placeholder="name" />
+            <button type="submit">Submit</button>
+          </form>
+          <h2>Existing contacts:</h2>
+          <ul>
+            {contacts.map((contact) =>
+              <li>{`Name: ${contact.name}`}</li>
+            )}
+          </ul>
+        </div>
+      )
+    }
+  }
+  
+
+  return(
+    <NameForm>
+      
+    </NameForm>
+  )
 }
 
 export default App;
